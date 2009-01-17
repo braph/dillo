a1e9c804062c1aed458021954a6de2c095d92e18
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2009-01-17 16:48:27 +0100

    implement parsing of CSS data from style attribute
    E.g.
    
    <body style="background-color: red; font-family: arial; font-size: 30mm">
    Hello World
    </body>

---
+++ src/menu.hh
@@ -0,0 +1,40 @@
+#ifndef __MENU_HH__
+#define __MENU_HH__
+
+#include "bw.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void a_Menu_page_popup(BrowserWindow *bw, const DilloUrl *url,
+                       bool_t has_bugs, bool_t unloaded_imgs);
+void a_Menu_link_popup(BrowserWindow *bw, const DilloUrl *url);
+void a_Menu_image_popup(BrowserWindow *bw, const DilloUrl *url,
+                        bool_t loaded_img, DilloUrl *link_url);
+void a_Menu_form_popup(BrowserWindow *bw, const DilloUrl *page_url,
+                       void *vform, bool_t showing_hiddens);
+void a_Menu_file_popup(BrowserWindow *bw, void *v_wid);
+void a_Menu_bugmeter_popup(BrowserWindow *bw, const DilloUrl *url);
+void a_Menu_history_popup(BrowserWindow *bw, int direction);
+
+//---------------------
+void a_Menu_popup_set_url(BrowserWindow *bw, const DilloUrl *url);
+void a_Menu_popup_set_url2(BrowserWindow *bw, const DilloUrl *url);
+void a_Menu_popup_clear_url2(void *menu_popup);
+
+DilloUrl *a_Menu_popup_get_url(BrowserWindow *bw);
+
+void a_Menu_pagemarks_new (BrowserWindow *bw);
+void a_Menu_pagemarks_destroy (BrowserWindow *bw);
+void a_Menu_pagemarks_add(BrowserWindow *bw, void *page, void *style,
+                          int level);
+void a_Menu_pagemarks_set_text(BrowserWindow *bw, const char *str);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* MENU_HH */
+
